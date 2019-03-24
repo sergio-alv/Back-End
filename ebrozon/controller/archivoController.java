aea9cd4c8a9554bc34e3949989336ee2525350fc
karny1998 <45491071+karny1998@users.noreply.github.com>
2019-03-24 20:20:14 +0100

    Add files via upload
    Versión avanzada de Usuario y controlador de archivo

---
+++ ebrozon/controller/archivoController.java
@@ -0,0 +1,75 @@
+package com.ebrozon.controller;
+
+import java.util.Optional;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+
+import com.ebrozon.model.archivo;
+import com.ebrozon.model.usuario;
+import com.ebrozon.repository.archivoRepository;
+
+@RestController
+public class archivoController {
+	
+	@Autowired
+    archivoRepository repository;
+	
+	private String upload_folder = ".//src//main//resources//files";
+	
+	@RequestMapping("/prueba")
+	public String prueba() {
+		archivo aux = new archivo("cwecewcec", 0);
+		repository.save(aux);
+		return "oK";
+	}
+	
+	@RequestMapping("/uploadFile")
+	public int uploadFile(@RequestParam("file") MultipartFile file) {
+		if(!file.isEmpty()) {
+			try {
+				byte[] array = new byte[10];
+				new Random().nextBytes(array);
+				String fileName = new String(array, Charset.forName("UTF-8")) + "-" + file.getOriginalFilename();
+				saveFile(file, fileName);
+				archivo f = new archivo(upload_folder + fileName, 0);
+				repository.save(f);
+				return repository.findByurl(upload_folder + fileName).get().getIdentificador();
+			}
+			catch(IOException e) {
+				return -1;
+			}
+		}
+		return -1;
+	}
+	
+	@RequestMapping("/loadFile")
+	public String loadFile(@RequestParam("identificador") int id) {
+		Optional<archivo> aux = repository.findByidentificador(id);
+		if(aux.isPresent()) {
+			return aux.get().getUrl();
+		}
+		return "Ha habido un problema con el archivo";
+	}
+	
+	private void saveFile(MultipartFile file, String filename) throws IOException {
+		if(!file.isEmpty()) {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(upload_folder + filename);
+			Files.write(path, bytes);
+		}
+	}
+}
--- ebrozon/controller/usuarioController.java
+++ ebrozon/controller/usuarioController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.security.MessageDigest;
 
-import com.ebrozon.model.Customer;
 import com.ebrozon.model.usuario;
 import com.ebrozon.repository.usuarioRepository;
 
@@ -19,11 +20,14 @@ public class usuarioController {
 	@Autowired
     usuarioRepository repository;
 	
+	@Autowired
+    archivoController archiver;
+	
 	@RequestMapping("/registrar")
     public String registrar(@RequestParam("un") String un, @RequestParam("cor") String cor, @RequestParam("pass") String pass,
     						@RequestParam("tel") int tel, @RequestParam("name") String na, @RequestParam("lna") String lna,
     						@RequestParam("cp") int cp, @RequestParam("ci") String ci, @RequestParam("pr") String pr,
-    						@RequestParam("lat") String lat, @RequestParam("lon") String lon, @RequestParam("im") String im
+    						@RequestParam("lat") String lat, @RequestParam("lon") String lon, @RequestParam("im") MultipartFile im
     						){
 		
 		if(repository.existsBynombreusuario(un)) {
@@ -52,14 +56,56 @@ public class usuarioController {
 		
 		try {
 	        user = new usuario(un, cor, sb.toString(), tel, na, lna, cp, ci, pr);
-	        user.setActivo(true);
+	        user.setActivo(1);
 	        
 	        if ((lat != null) && (!lat.trim().equals("")) && (lon != null) && (!lon.trim().equals(""))){ 
 	        	user.setLatitud(Float.parseFloat(lat));
 	        	user.setLongitud(Float.parseFloat(lon));
 	        }
-	        if ((im != null) && (!im.trim().equals(""))){ 
+	        if (!im.isEmpty()){ 
+	        	int idIm = archiver.uploadFile(im);
+	        	user.setArchivo(idIm);
+	        }
+	        else {
 	        	user.setArchivo(0);
+	        }
+		}
+		catch(Exception e){
+			return e.getMessage();
+		}
+        
+        repository.save(user);
+        
+        return "Ok";
+    }
+	
+	@RequestMapping("/actualizarUsuario")
+    public String actualizarUsuario(@RequestParam("un") String un,
+    						@RequestParam("tel") int tel, @RequestParam("name") String na, @RequestParam("lna") String lna,
+    						@RequestParam("cp") int cp, @RequestParam("ci") String ci, @RequestParam("pr") String pr,
+    						@RequestParam("lat") String lat, @RequestParam("lon") String lon, @RequestParam("im") MultipartFile im
+    						){
+		
+		if(!repository.existsBynombreusuario(un)) {
+			return "El nombre de usuario no exite.";
+		}
+		
+		usuario user = repository.findBynombreusuario(un).get();
+		
+		try {
+			user.setTelefono(tel);
+			user.setNombre(na);
+	        user.setApellidos(lna);
+	        user.setProvincia(pr);
+	        user.setCodigopostal(cp);
+	        user.setCiudad(ci);
+	        if ((lat != null) && (!lat.trim().equals("")) && (lon != null) && (!lon.trim().equals(""))){ 
+	        	user.setLatitud(Float.parseFloat(lat));
+	        	user.setLongitud(Float.parseFloat(lon));
+	        }
+	        if (!im.isEmpty()){ 
+	        	int idIm = archiver.uploadFile(im);
+	        	user.setArchivo(idIm);
 	        }
 		}
 		catch(Exception e){
@@ -107,11 +153,40 @@ public class usuarioController {
 	public usuario recuperarUsuario(@RequestParam("un") String un) {
 		Optional<usuario> aux = repository.findBynombreusuario(un);
 		if(aux.isPresent()) {
-			return aux.get();
+			usuario user = aux.get();
+			user.setUrlArchivo(archiver.loadFile(user.getArchivo()));
+			return user;
 		}
 		else {
 			return null;
 		}
 	}
 	
+	@RequestMapping("/banearUsuario")
+	public String banearUsuario(@RequestParam("un") String un) {
+		Optional<usuario> aux = repository.findBynombreusuario(un);
+		if(aux.isPresent()) {
+			usuario user = aux.get();
+			user.setActivo(0);
+			repository.save(user);
+			return "Ok";
+		}
+		else {
+			return "No se ha encontrado el usuario";
+		}
+	}
+	
+	@RequestMapping("/desbanearUsuario")
+	public String desbanearUsuario(@RequestParam("un") String un) {
+		Optional<usuario> aux = repository.findBynombreusuario(un);
+		if(aux.isPresent()) {
+			usuario user = aux.get();
+			user.setActivo(1);
+			repository.save(user);
+			return "Ok";
+		}
+		else {
+			return "No se ha encontrado el usuario";
+		}
+	}
 }
