01a412da02a89a3aae22107f8d33110989ab139f
Eduardo Gimeno <34724940+Edu7216@users.noreply.github.com>
2019-03-24 20:20:44 +0100

    Add files via upload

---
+++ ebrozon/repository/usuarioverantRepository.java
@@ -0,0 +1,16 @@
+package com.ebrozon.repository;
+ 
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.ebrozon.model.usuarioverant;
+
+public interface usuarioverantRepository extends CrudRepository<usuarioverant, Long>{
+	
+}
