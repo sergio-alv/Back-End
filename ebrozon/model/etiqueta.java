9d2ecaadc2af1f21aa03b967cafbfee1cd295d5a
Eduardo Gimeno <34724940+Edu7216@users.noreply.github.com>
2019-03-31 20:32:43 +0200

    Add files via upload

--- ebrozon/model/etiqueta.java
+++ ebrozon/model/etiqueta.java
@@ -16,11 +16,13 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "usuarioverant")
+@Table(name = "etiqueta")
 public class etiqueta {
 	
 	@Id
 	@Column(name = "nombre")
+	@Size(min = 3, max = 30, message="El nombre de la etiqueta tiene que tener entre 3 y 30 caracteres")
+	@Pattern(regexp = "[A-z,0-9,_,-]+", message="El nombre de la etiqueta solo puede tener letras mayúsculas o minúsculas sin acentuar, números, y los caracteres _ y -")
 	private String nombre;
 	
 	@Column(name = "fechacreacion")
@@ -28,8 +30,43 @@ public class etiqueta {
 	private Date fechacreacion;
 	
 	@Column(name = "nombreusuario")
-	@Size(min = 3, max = 30, message="El nombre tiene que tener entre 3 y 30 caracteres")
-	@Pattern(regexp = "[A-z,0-9,_,-]+", message="El nombre solo puede tener letras mayúsculas o minúsculas sin acentuar, números, y los caracteres _ y -")
+	@Size(min = 3, max = 30, message="El nombre del creador tiene que tener entre 3 y 30 caracteres")
+	@Pattern(regexp = "[A-z,0-9,_,-]+", message="El nombre del creador solo puede tener letras mayúsculas o minúsculas sin acentuar, números, y los caracteres _ y -")
 	private String creador;
-	 
+	
+	public etiqueta(
+			@Size(min = 3, max = 30, message="El nombre de la etiqueta tiene que tener entre 3 y 30 caracteres") @Pattern(regexp = "[A-z,0-9,_,-]+", message="El nombre de la etiqueta solo puede tener letras mayúsculas o minúsculas sin acentuar, números, y los caracteres _ y -") String nombre,
+			@NotNull Date fechacreacion,
+			@Size(min = 3, max = 30, message="El nombre del creador tiene que tener entre 3 y 30 caracteres") @Pattern(regexp = "[A-z,0-9,_,-]+", message="El nombre del creador solo puede tener letras mayúsculas o minúsculas sin acentuar, números, y los caracteres _ y -") String creador) {
+		super();
+		this.nombre = nombre;
+		this.fechacreacion = fechacreacion;
+		this.creador = creador;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechacreacion() {
+		return fechacreacion;
+	}
+
+	public void setFechacreacion(Date fechacreacion) {
+		this.fechacreacion = fechacreacion;
+	}
+
+	public String getCreador() {
+		return creador;
+	}
+
+	public void setCreador(String creador) {
+		this.creador = creador;
+	}
+	
+	
 }
