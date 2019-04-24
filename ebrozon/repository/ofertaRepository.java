2c3d104be7e840cf0b023233b507c34b96eb2429
Eduardo Gimeno <34724940+Edu7216@users.noreply.github.com>
2019-04-24 13:52:21 +0200

    Add files via upload

---
+++ ebrozon/repository/ofertaRepository.java
@@ -0,0 +1,23 @@
+package com.ebrozon.repository;
+ 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ebrozon.model.oferta;
+
+public interface ofertaRepository extends CrudRepository<oferta, Long>{
+	boolean existsByusuarioAndcantidadAndproducto(String usuario, Float cantidad, String producto);
+	
+	List<oferta> findBynventaOrderByFechaDesc(int nventa);
+	
+	List<oferta> findByusuarioOrderByFechaDesc(String usuario);
+	
+	List<oferta> findByproductoOrderByFechaDesc(String producto);
+	
+	boolean existsByusuarioAndnventaAndfechaAndcantidad(String usuario, int nventa, Date fecha, Float cantidad);
+	
+	Optional<oferta> findByusuarioAndNventaAndFechaAndCantidad(String usuario, int nventa, Date fecha, Float cantidad);
+}
