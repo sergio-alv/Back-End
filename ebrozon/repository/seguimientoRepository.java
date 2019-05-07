f79e036e26e2d86471bf7d1e8a8be47fd4ef0fa3
dranter13 <43911245+dranter13@users.noreply.github.com>
2019-05-07 15:57:10 +0200

    Update seguimientoRepository.java

--- ebrozon/repository/seguimientoRepository.java
+++ ebrozon/repository/seguimientoRepository.java
@@ -4,22 +4,30 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ebrozon.model.seguimiento;
 
 public interface seguimientoRepository extends CrudRepository<seguimiento, Long>{
-	boolean existsByusuarioAndnventaAndproducto(String usuario, int nventa, String producto);
+	boolean existsByusuarioAndNventaAndProducto(String usuario, int nventa, String producto);
+    
+    	boolean existsByidentificador(int id);
+    
+    	Optional<seguimiento> findByidentificador(int id);
   
-  List<seguimiento> findBynventaOrderByFechaDesc(int nventa);
+  	List<seguimiento> findBynventaOrderByFechaDesc(int nventa);
 	
 	List<seguimiento> findByusuarioOrderByFechaDesc(String usuario);
 	
 	List<seguimiento> findByproductoOrderByFechaDesc(String producto);
     
-	Optional<seguimiento> findByusuarioAndNventaAndproducto(String usuario, int nventa, String producto);
+	Optional<seguimiento> findByusuarioAndNventaAndProducto(String usuario, int nventa, String producto);
   
-  boolean existsByusuarioAndnventaAndfecha(String usuario, int nventa, Date fecha);
+  	//boolean existsByusuarioAndNventaAndFecha(String usuario, int nventa, Date fecha);
 	
-	Optional<seguimiento> findByusuarioAndNventaAndFecha(String usuario, int nventa, Date fecha);
+	//Optional<seguimiento> findByusuarioAndNventaAndFecha(String usuario, int nventa, Date fecha);
+	
+	@Query("Select max(identificador) from seguimiento")
+	Optional<Integer> lastId();
 }
