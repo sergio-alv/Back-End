dec5bfc95fee52bbe3286a1fbb10324cc0f7c322
karny1998 <45491071+karny1998@users.noreply.github.com>
2019-05-16 21:15:06 +0200

    Add files via upload

--- ebrozon/model/etiqueta.java
+++ ebrozon/model/etiqueta.java
@@ -1,17 +1,12 @@
 package com.ebrozon.model;
 
-import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -21,26 +16,24 @@ public class etiqueta {
 	
 	@Id
 	@Column(name = "nombre")
-	@Size(min = 3, max = 30, message="El nombre de la etiqueta tiene que tener entre 3 y 30 caracteres")
-	@Pattern(regexp = "[A-z,0-9,_,-]+", message="El nombre de la etiqueta solo puede tener letras mayúsculas o minúsculas sin acentuar, números, y los caracteres _ y -")
 	private String nombre;
 	
 	@Column(name = "fechacreacion")
 	@NotNull
 	private Date fechacreacion;
 	
-	@Column(name = "nombreusuario")
+	@Column(name = "creador")
 	@Size(min = 3, max = 30, message="El nombre del creador tiene que tener entre 3 y 30 caracteres")
 	@Pattern(regexp = "[A-z,0-9,_,-]+", message="El nombre del creador solo puede tener letras mayúsculas o minúsculas sin acentuar, números, y los caracteres _ y -")
 	private String creador;
 	
-	public etiqueta(
-			@Size(min = 3, max = 30, message="El nombre de la etiqueta tiene que tener entre 3 y 30 caracteres") @Pattern(regexp = "[A-z,0-9,_,-]+", message="El nombre de la etiqueta solo puede tener letras mayúsculas o minúsculas sin acentuar, números, y los caracteres _ y -") String nombre,
-			@NotNull Date fechacreacion,
+	public etiqueta() {}
+	
+	public etiqueta(String nombre,
 			@Size(min = 3, max = 30, message="El nombre del creador tiene que tener entre 3 y 30 caracteres") @Pattern(regexp = "[A-z,0-9,_,-]+", message="El nombre del creador solo puede tener letras mayúsculas o minúsculas sin acentuar, números, y los caracteres _ y -") String creador) {
 		super();
 		this.nombre = nombre;
-		this.fechacreacion = fechacreacion;
+		this.fechacreacion = new Date();
 		this.creador = creador;
 	}
 
