280f07a80e53f76559183f00a528853278922d32
karny1998 <45491071+karny1998@users.noreply.github.com>
2019-05-22 19:12:38 +0200

    Add files via upload

--- ebrozon/com/ebrozon/controller/usuarioController.java
+++ ebrozon/com/ebrozon/controller/usuarioController.java
@@ -49,16 +49,16 @@ public class usuarioController {
 		   Properties props = new Properties();
 		   props.put("mail.smtp.auth", "true");
 		   props.put("mail.smtp.starttls.enable", "true");
-		   props.put("mail.smtp.host", "smtp.sendgrid.net");
+		   props.put("mail.smtp.host", "smtp.gmail.com");
 		   props.put("mail.smtp.port", "587");
 		   
 		   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 		      protected PasswordAuthentication getPasswordAuthentication() {
-		         return new PasswordAuthentication("app129790258@heroku.com", "ygg5o74m5219");
+		         return new PasswordAuthentication("karny.sac@gmail.com", "199819981998s");
 		      }
 		   });
 		   Message msg = new MimeMessage(session);
-		   msg.setFrom(new InternetAddress("app129790258@heroku.com", false));
+		   msg.setFrom(new InternetAddress("karny.sac@gmail.com", false));
 
 		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(cor));
 		   msg.setSubject("Registro en Ebrozon");
@@ -144,7 +144,7 @@ public class usuarioController {
 	        }
 	        
 	        user.setActivo(1);
-	        sendmail(user.getNombreusuario(),user.getContrasena(), user.getCorreo());
+	        //sendmail(user.getNombreusuario(),user.getContrasena(), user.getCorreo());
 	        repository.save(user);
 		}
 		catch(Exception e) {
@@ -474,16 +474,16 @@ public class usuarioController {
 		   Properties props = new Properties();
 		   props.put("mail.smtp.auth", "true");
 		   props.put("mail.smtp.starttls.enable", "true");
-		   props.put("mail.smtp.host", "smtp.sendgrid.net");
+		   props.put("mail.smtp.host", "smtp.gmail.com");
 		   props.put("mail.smtp.port", "587");
 		   
 		   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 		      protected PasswordAuthentication getPasswordAuthentication() {
-		         return new PasswordAuthentication("app129790258@heroku.com", "ygg5o74m5219");
+		         return new PasswordAuthentication("karny.sac@gmail.com", "199819981998s");
 		      }
 		   });
 		   Message msg = new MimeMessage(session);
-		   msg.setFrom(new InternetAddress("app129790258@heroku.com", false));
+		   msg.setFrom(new InternetAddress("karny.sac@gmail.com", false));
 
 		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(cor));
 		   msg.setSubject("Recuperación de contraseña en Ebrozon");
@@ -527,7 +527,7 @@ public class usuarioController {
 		    us.setContrasena(sb.toString());
 		    repository.save(us);
 		    try {
-		    	sendmail2(us.getNombreusuario(),newpass,us.getCorreo());
+		    	//sendmail2(us.getNombreusuario(),newpass,us.getCorreo());
 		    }
 		    catch(Exception e) {
 		    	us.setContrasena(oldpass);
